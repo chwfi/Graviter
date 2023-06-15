344909e02f6e1d2ad56f74b6a5e70a4481876984
9mean3 <gminseong114@gmail.com>
2023-06-15 18:59:55 +0900

    d

--- Assets/MinSeong/01.Scripts/MainMenuButtons.cs
+++ Assets/MinSeong/01.Scripts/MainMenuButtons.cs
@@ -5,45 +5,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class MainMenuButtons : MonoBehaviour
 {
-    public Button[] buttons;
-    public Image[] images;
-    RectTransform RectTransform;
-    int index = 0;
 
-    [SerializeField] float duration = 2;
-    [SerializeField] Transform zero;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (index >= buttons.Length - 1)
-                index = 0;
-            else
-                index++;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (index <= 0)
-                index = buttons.Length - 1;
-            else
-                index--;
-        }
-
-        UpdateButton();
-    }
-
-    void UpdateButton()
-    {
-        buttons[index].transform.; //Vector3.Lerp(buttons[index].transform.position, Vector3.zero, duration);
-        
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            //buttons[i].transform.position = Vector3.Lerp(buttons[i].transform.position, new Vector3(0, 100*(i - index)), duration);
-            buttons[i].transform.DOMove(Vector3.up * 150 * (i - index), duration);
-        }
-    }
 }
