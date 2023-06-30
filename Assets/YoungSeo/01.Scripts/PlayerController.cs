2bcb6bef8de2c0c57b7007e57297ce48e9aa5be9
leeyoungseo6 <dudsj9782@gmail.com>
2023-06-30 10:11:47 +0900

    스테이지 15, Q누르면 도착위치 화살표로 표시

--- Assets/YoungSeo/01.Scripts/PlayerController.cs
+++ Assets/YoungSeo/01.Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,15 +13,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _jumpPower = 5f;
 
+    private Transform _arrowTrm;
+    private SpriteRenderer _arrowRenderer;
+
+    private Vector2 _clearPos;
+
     //[SerializeField]
     //private LayerMask Ground;
     //private bool _isGround;
 
-    private float _gravityDir;
-
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _clearPos = GameObject.Find("Clear").transform.position;
+        _arrowTrm = transform.Find("Arrow");
+        _arrowRenderer = _arrowTrm.Find("ArrowVisual").GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FindClearPosition());
     }
 
     private void Update()
@@ -49,6 +61,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigid.AddForce(Vector3.up * _jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    IEnumerator FindClearPosition()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _arrowRenderer.color = Color.red;
+                float z = Mathf.Atan2(_clearPos.y - _arrowTrm.position.y, _clearPos.x - _arrowTrm.position.x) * Mathf.Rad2Deg - 90;
+                _arrowTrm.rotation = Quaternion.Euler(0, 0, z);
+                _arrowRenderer.DOFade(0, 1f).SetEase(Ease.InCubic);
+                yield return new WaitForSeconds(1f);
+            }
+            yield return null;
         }
     }
 }
