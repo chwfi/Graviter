2aa82394097987b0d70fbf613db5d9eb83b74f6f
mingyo0125 <wjdalsry125@gmail.com>
2023-07-22 15:45:44 +0900

    인게임esc기능만구현

---
+++ Assets/Mingyo/01.Scripts/UI/UIController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UIElements;
+using DG.Tweening.Core;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform settingPanel;
+
+    [SerializeField]
+    private float defaultTrmY;
+
+    bool isOn = false;
+
+    private void Awake()
+    {
+        defaultTrmY = settingPanel.rect.height + 30;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SettingOnOff();
+        }
+    }
+
+    public void SettingOnOff()
+    {
+        settingPanel.DOKill();
+
+        if(isOn)
+        {
+            settingPanel.DOAnchorPosY(defaultTrmY, 1f).SetEase(Ease.OutExpo).SetUpdate(true); //On
+        }
+        else
+        {
+            settingPanel.DOAnchorPosY(defaultTrmY - 1080, 1f).SetEase(Ease.OutExpo).SetUpdate(true); //Off
+
+        }
+
+        isOn = !isOn;
+    }
+
+
+
+}
