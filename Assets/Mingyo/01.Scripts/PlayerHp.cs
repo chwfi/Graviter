4f1208bd5be87eb2ff43719fb81ac4fa3514246d
mingyo0125 <wjdalsry125@gmail.com>
2023-10-07 21:46:04 +0900

    주금

---
+++ Assets/Mingyo/01.Scripts/PlayerHp.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHp : MonoBehaviour
+{
+    public UnityEvent OnDie;
+
+    Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "SqBoss":
+                OnDie?.Invoke();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Die()
+    {
+        _animator.SetBool("IsDie", true);
+    }
+
+}
