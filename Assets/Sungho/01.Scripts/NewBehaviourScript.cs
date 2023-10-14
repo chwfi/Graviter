4e23d63c54399cf44544b206489c41e6aad76291
rlatjdgh1122 <rlatjdgh2358@naver.com>
2023-10-15 00:24:47 +0900

    블랙홀 쉐이더

--- Assets/Sungho/01.Scripts/NewBehaviourScript.cs
+++ Assets/Sungho/01.Scripts/NewBehaviourScript.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    public void Start()
-    {
-    }
 }
